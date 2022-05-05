767b1e40e006149c56d518db8feb877ce9fcdec4
Asim-Coding <muhammadasim19955@gmail.com>
2022-05-05 12:38:45 +0500

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+console.log("synchronous programming");
+function myfunc(){
+  console.log("inside function");
+}
+console.log("new function");
+myfunc();
+console.log("my function");
+
+
+console.log("Anychronous programming");
+
+console.log("Start")
+setTimeout(()=>{
+   
+})
